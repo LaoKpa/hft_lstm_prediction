4e0eb3b1c84054887cd3b4cfe98e54576a732873
Tiago Kramer Vieira <tiagokv@hotmail.com>
2016-03-27 10:46:01 -0300

    Main using custom brick

--- hft_lstm/__init__.py
+++ hft_lstm/__init__.py
@@ -3,8 +3,7 @@ from __future__ import print_function
 
 from blocks.bricks import Linear
 from blocks.bricks.recurrent import LSTM
-from blocks.bricks.cost import SquaredError
-from blocks.initialization import Constant, IsotropicGaussian
+
 from blocks.algorithms import GradientDescent, Adam
 from blocks.graph import ComputationGraph
 from blocks.extensions import FinishAfter, Printing, ProgressBar
@@ -14,10 +13,14 @@ from blocks.extensions.monitoring import (DataStreamMonitoring,
 from blocks.main_loop import MainLoop
 from blocks.model import Model
 
+import theano
 import theano.tensor as T
 import numpy as np
 import pdb
-from converters import BatchStreamConverter
+
+import converters
+from cost import AbsolutePercentageError
+from custom_bricks import LinearLSTM
 
 try:
     from blocks_extras.extensions.plot import Plot
@@ -35,7 +38,8 @@ def main(save_path, data_path, lstm_dim, batch_size, num_epochs):
 
     save_file = save_path + execution_name
 
-    converter = BatchStreamConverter(data_path + 'dados_petr.csv')
+    # converter = converters.BatchStreamConverter(data_path + 'dados_petr.csv')
+    converter = converters.IterStreamConverter(data_path + 'dados_petr.csv')
     converter.load()
 
     stream_train, stream_test = converter.get_streams(batch_size)
@@ -43,54 +47,40 @@ def main(save_path, data_path, lstm_dim, batch_size, num_epochs):
     x = T.tensor3('x')
     y = T.tensor3('y')
 
-    # we need to provide data for the LSTM layer of size 4 * lstm_dim, see
-    # LSTM layer documentation for the explanation
-    x_to_h = Linear(6, lstm_dim * 4, name='x_to_h',
-                    weights_init=IsotropicGaussian(),
-                    biases_init=Constant(0.0))
-    lstm = LSTM(lstm_dim, name='lstm',
-                weights_init=IsotropicGaussian(),
-                biases_init=Constant(0.0))
-    h_to_o = Linear(lstm_dim, 1, name='h_to_o',
-                    weights_init=IsotropicGaussian(),
-                    biases_init=Constant(0.0))
+    linear_lstm = LinearLSTM(6, 1, lstm_dim)
 
-    x_transform = x_to_h.apply(x)
-    h, c = lstm.apply(x_transform)
-    y_hat = h_to_o.apply(h)
-    y_hat.name = 'y_hat'
-
-    lstm.initialize()
-    x_to_h.initialize()
-    h_to_o.initialize()
+    y_hat = linear_lstm.apply(x)
+    linear_lstm.initialize()
 
     # start of testing
     # f = theano.function([x], y_hat)
-    #
+
     # for data in stream_test.get_epoch_iterator():
     #     print('x shape: {} , y shape: {}'.format(data[0].shape, data[1].shape))
-    #     y_test = f(data[0])
-    #     print("y_test {}".format(y_test))
-    #     print("y shape {}".format(y_test.shape))
+    #     # y_test = f(data[0])
+    #     # print("y_test {}".format(y_test))
+    #     # print("y shape {}".format(y_test.shape))
     #     return
     # end of testing
 
-    cost = SquaredError().apply(y, y_hat)
-    cost.name = 'cost'
+    c = AbsolutePercentageError().apply(y, y_hat)
+    c.name = 'cost'
 
-    cg = ComputationGraph(cost)
+    cg = ComputationGraph(c)
 
-    algorithm = GradientDescent(cost=cost, parameters=cg.parameters, step_rule=Adam())
-    test_monitor = DataStreamMonitoring(variables=[cost], data_stream=stream_test, prefix='test')
-    train_monitor = TrainingDataMonitoring(variables=[cost], prefix='train', after_epoch=True)
+    algorithm = GradientDescent(cost=c, parameters=cg.parameters, step_rule=Adam())
+    test_monitor = DataStreamMonitoring(variables=[c], data_stream=stream_test, prefix='test')
+    train_monitor = TrainingDataMonitoring(variables=[c], prefix='train', after_epoch=True)
 
     if BOKEH_AVAILABLE:
         plot = Plot(execution_name, channels=[['train_cost', 'test_cost']])
 
-    main_loop = MainLoop(algorithm, stream_train, model=Model(cost),
+    main_loop = MainLoop(algorithm, stream_train, model=Model(c),
                          extensions=[test_monitor, train_monitor,
                                      FinishAfter(after_n_epochs=num_epochs),
-                                     Printing(), ProgressBar(), Checkpoint(save_file), plot])
+                                     Printing(), ProgressBar(),
+                                     Checkpoint(save_file),
+                                     plot])
     main_loop.run()
 
     print('If you reached here, you have a trained LSTM :)')
